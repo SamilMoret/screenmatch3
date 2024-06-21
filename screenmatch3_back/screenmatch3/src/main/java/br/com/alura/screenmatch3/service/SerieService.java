2cf578c0a0ff8bcded8ea688975d3f4d29df6d66
Samil <samil.moret@gmail.com>
2024-06-21 09:13:01 -0300

    utilizando um serviço e mostrando a página inicial

--- screenmatch3_back/screenmatch3/src/main/java/br/com/alura/screenmatch3/service/SerieService.java
+++ screenmatch3_back/screenmatch3/src/main/java/br/com/alura/screenmatch3/service/SerieService.java
@@ -2,11 +2,13 @@ package br.com.alura.screenmatch3.service;
 
 
 import br.com.alura.screenmatch3.dto.SerieDTO;
+import br.com.alura.screenmatch3.model.Serie;
 import br.com.alura.screenmatch3.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,11 +18,37 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries() {
-        return repositorio.findAll()
-                .stream()
+        return converteDados(repositorio.findAll());
+
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect( Collectors.toList());
+                .collect(Collectors.toList());
+    }
+
+
+    public List<SerieDTO> obterLancamentos () {
+        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obterPorId (Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
+
+
 
 
